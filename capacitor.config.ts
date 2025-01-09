b5a21a31a27793108ab5e2dd46e34e70fa8c0b09
antonella <ant.valdesr@duoocuc.cl>
2025-01-08 21:31:32 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'helpPets',
+  webDir: 'www'
+};
+
+export default config;
