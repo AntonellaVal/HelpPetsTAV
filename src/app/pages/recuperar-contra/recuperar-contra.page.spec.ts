f6d440a9bea17ff5490567b495d2e4b89f46c2c8
gianinna <gi.causa@duocuc.cl>
2025-01-29 18:35:45 -0300

    pruebas listas con el agregado de la pagina recuperar contra

--- src/app/pages/recuperar-contra/recuperar-contra.page.spec.ts
+++ src/app/pages/recuperar-contra/recuperar-contra.page.spec.ts
@@ -1,11 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RecuperarContraPage } from './recuperar-contra.page';
+import { IonicModule } from '@ionic/angular';
+import { SQLite } from '@awesome-cordova-plugins/sqlite/ngx';
 
 describe('RecuperarContraPage', () => {
   let component: RecuperarContraPage;
   let fixture: ComponentFixture<RecuperarContraPage>;
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [RecuperarContraPage],
+      imports: [IonicModule.forRoot()],
+      providers: [SQLite]
+    }).compileComponents();
     fixture = TestBed.createComponent(RecuperarContraPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
